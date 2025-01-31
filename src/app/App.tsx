7c128267c1eb2c2452e07b2f817cc3e87572289f
Nikita Malofeev <front.work3@gmail.com>
2025-01-31 21:31:56 +0300

    пробую директивное назначение vh после calc

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import AppRouter from './providers/router/ui/AppRouter'
 import './styles/index.scss'
@@ -6,6 +6,12 @@ import { Header } from 'widgets/Header/ui/Header'
 import { Cover } from 'shared/ui/Cover/Cover'
 
 function App() {
+
+  useEffect(() => {
+    const userVh = window.innerHeight / 100
+    document.documentElement.style.setProperty('--vh', `${userVh}px`)
+  }, [])
+  
   return (
     <div className='page__wrapper'>
       <Header />

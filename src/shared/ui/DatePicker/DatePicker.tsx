8b2f122b6b792ebf51ef3f7644f575a84da99b1c
Nikita Malofeev <front.work3@gmail.com>
2025-04-01 19:37:31 +0300

    +

--- src/shared/ui/DatePicker/DatePicker.tsx
+++ src/shared/ui/DatePicker/DatePicker.tsx
@@ -59,18 +59,6 @@ export const Datepicker = memo((props: DatepickerProps) => {
         setIsOpen(false);
     };
 
-    useEffect(() => {
-        if (isOpen && /iPhone|iPad|iPod/.test(navigator.userAgent)) {
-            document.body.style.position = "fixed";
-            document.body.style.top = `-${window.scrollY}px`;
-        } else {
-            const scrollY = document.body.style.top;
-            document.body.style.position = '';
-            document.body.style.top = '';
-            if (scrollY) window.scrollTo(0, parseInt(scrollY || '0') * -1);
-        }
-    }, [isOpen]);
-
     // Клик вне календаря — закрывает календарь
     useEffect(() => {
         function handleClickOutside(event: MouseEvent) {

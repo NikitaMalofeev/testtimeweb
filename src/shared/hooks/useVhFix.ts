0025f39f1427bf7e286115086838c44954bb6744
Nikita Malofeev <front.work3@gmail.com>
2025-08-11 16:59:51 +0300

    +

--- src/shared/hooks/useVhFix.ts
+++ src/shared/hooks/useVhFix.ts
@@ -45,6 +45,7 @@ export function useVhFix(opts: Opts = { observeBody: true }) {
                 attributeFilter: ['class', 'style'],
             });
         }
+        //
 
         return () => {
             clearTimeout(t);
